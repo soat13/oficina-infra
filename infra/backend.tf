1eb2799de91018ac199917bfd9c1e137bdce4f3f
priolilucas1 <priolilucas1@gmail.com>
2025-12-15 22:08:53 -0300

    feat: base ci-cd pipeline

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "soatfiap.tf"
-    key     = "oficina-infra/terraform.tfstate"
-    region  = "us-east-1"
+    bucket = "soatfiap.tf"
+    key    = "oficina-infra/terraform.tfstate"
+    region = "us-east-1"
   }
 }
