c92f611698645ee9e5c89a1b124a5e613385cb6e
Marcos Souza <marcossouza@msouza.com>
2025-12-21 19:34:06 -0300

    Deploy infra

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "soatfiap.tf"
-    key    = "oficina-infra/terraform.tfstate"
+    bucket = "soatfiap-f2"
+    key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
 }
