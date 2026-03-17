57fd7b47cc9f819c3ebec55ca12539743c13dbd0
Marcos Souza <asxpyre@outlook.com>
2026-03-17 19:15:39 -0300

    chore: update sg eks

--- infra/modules/eks-cluster/main.tf
+++ infra/modules/eks-cluster/main.tf
@@ -136,3 +136,13 @@ resource "aws_eks_cluster" "main" {
   tags = var.tags
 }
 
+# Security Group Rule: Allow VPC CIDR to auto-generated EKS cluster security group
+resource "aws_security_group_rule" "eks_managed_sg_vpc_cidr" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = [var.vpc_cidr]
+  security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  description       = "Allow VPC CIDR to auto-generated EKS cluster security group"
+}
