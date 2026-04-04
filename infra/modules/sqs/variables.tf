d03163cac27266cd86ffa0251a6b644e43828c57
Marcos Souza <asxpyre@outlook.com>
2026-04-04 13:57:12 -0300

    feat: Add: SQS  Module

---
+++ infra/modules/sqs/variables.tf
@@ -0,0 +1,59 @@
+variable "name" {
+  description = "Name of the SQS queue"
+  type        = string
+}
+
+
+variable "delay_seconds" {
+  description = "The time in seconds that the delivery of all messages in the queue will be delayed"
+  type        = number
+  default     = 0
+}
+
+variable "max_message_size" {
+  description = "The limit of how many bytes a message can contain before Amazon SQS rejects it"
+  type        = number
+  default     = 262144
+}
+
+variable "message_retention_seconds" {
+  description = "The number of seconds Amazon SQS retains a message"
+  type        = number
+  default     = 345600
+}
+
+variable "receive_wait_time_seconds" {
+  description = "The time for which a ReceiveMessage call will wait for a message to arrive"
+  type        = number
+  default     = 0
+}
+
+variable "visibility_timeout_seconds" {
+  description = "The visibility timeout for the queue"
+  type        = number
+  default     = 30
+}
+
+variable "kms_master_key_id" {
+  description = "The ID of an AWS-managed customer master key (CMK) for Amazon SQS or a custom CMK"
+  type        = string
+  default     = null
+}
+
+variable "create_dlq" {
+  description = "Determines whether to create a Dead Letter Queue (DLQ)"
+  type        = bool
+  default     = false
+}
+
+variable "max_receive_count" {
+  description = "The maximum number of times that a message can be received by consumers"
+  type        = number
+  default     = 5
+}
+
+variable "tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
