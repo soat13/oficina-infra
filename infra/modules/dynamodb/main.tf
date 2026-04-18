fd0bd3ba7874a148457f9e533daeb16c97e40738
Marcos Souza <asxpyre@outlook.com>
2026-04-17 22:35:50 -0300

    feat: auth

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "this" {
-  name           = var.name
-  billing_mode   = var.billing_mode
-  hash_key       = var.hash_key
-  range_key      = var.range_key
+  name         = var.name
+  billing_mode = var.billing_mode
+  hash_key     = var.hash_key
+  range_key    = var.range_key
 
   dynamic "attribute" {
     for_each = var.attributes
